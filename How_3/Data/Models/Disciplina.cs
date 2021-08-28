84a666597089e3171f99a177eb11b7b3b31fa982
Gabriel Maurici <gabriel.maurici@hotmail.com>
2021-08-28 15:10:16 -0300

    Add Projeto Crud e Data com Classe Disciplina

---
+++ How_3/Data/Models/Disciplina.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class Disciplina
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string NomeCompleto { get; set; }
+        public string NivelEnsino { get; set; }
+        public int CreditosAcademicos { get; set; }
+        public int CreditosFinanceiros { get; set; }
+        public int CargaHoraria { get; set; }
+        public int DuracaoAula { get; set; }
+    }
+}
